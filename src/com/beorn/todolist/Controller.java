eff4f01392e277ce9006fdd2b7fbda246eeff9b0
BeornPL <37457935+BeornPL@users.noreply.github.com>
2018-03-20 23:57:54 +0100

    7th video: - DialogPane, DialogController, Dialog.fxml

--- src/com/beorn/todolist/Controller.java
+++ src/com/beorn/todolist/Controller.java
@@ -5,12 +5,11 @@ import com.beorn.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
@@ -27,28 +26,10 @@ public class Controller {
     private TextArea itemDetailsTextArea;
     @FXML
     private Label deadlineLabel;
+    @FXML
+    private BorderPane mainBorderPane;
 
     public void initialize() {
-//        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
-//                LocalDate.of(2018, Month.APRIL, 25));
-//        TodoItem item2 = new TodoItem("Doctor's appointment", "See Dr. Smith at 123 Main Street. Bring paperwork.",
-//                LocalDate.of(2018, Month.MAY, 23));
-//        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website mockups by Friday 22nd April",
-//                LocalDate.of(2018, Month.APRIL, 22));
-//        TodoItem item4 = new TodoItem("Pickup Doug", "Doug's arrives on March 18 on the 5:00 train",
-//                LocalDate.of(2018, Month.MARCH, 18));
-//        TodoItem item5 = new TodoItem("Pick up dry cleaning", "Clothes should be ready by Wednesday",
-//                LocalDate.of(2018, Month.MARCH, 21));
-//
-//        todoItems = new ArrayList<TodoItem>();
-//        todoItems.add(item1);
-//        todoItems.add(item2);
-//        todoItems.add(item3);
-//        todoItems.add(item4);
-//        todoItems.add(item5);
-//
-//        TodoData.getInsance().setTodoItems(todoItems);
-
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
             public void changed(ObservableValue<? extends TodoItem> observable, TodoItem oldValue, TodoItem newValue) {
@@ -67,14 +48,19 @@ public class Controller {
     }
 
     @FXML
+    public void showNewItemDialoge() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        try{
+
+        } catch(IOException e) {
+            System.out.println("Couldn't load the dialog");
+        }
+    }
+
+    @FXML
     public void handleClickListView() {
         TodoItem item = todoListView.getSelectionModel().getSelectedItem();
-//        System.out.println("The selected item is " + item);
-//        StringBuilder sb = new StringBuilder(item.getDetails());
-//        sb.append("\n\n\n\n");
-//        sb.append("Due: ");
-//        sb.append(item.getDeadline().toString());
-//        itemDetailsTextArea.setText(sb.toString());
         itemDetailsTextArea.setText(item.getDetails());
         deadlineLabel.setText(item.getDeadline().toString());
     }
---
+++ src/com/beorn/todolist/DialogController.java
@@ -0,0 +1,7 @@
+package com.beorn.todolist;
+
+/**
+ * Created by Beorn on 2018-03-20.
+ */
+public class DialogController {
+}
