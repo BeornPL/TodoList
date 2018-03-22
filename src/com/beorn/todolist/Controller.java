6d9ea6e128b264e3479391e781590749f9d66d9f
BeornPL <37457935+BeornPL@users.noreply.github.com>
2018-03-22 23:43:56 +0100

    10th video: - DataBinding and Observable Collections - wrapText - array out of bounds??

--- src/com/beorn/todolist/Controller.java
+++ src/com/beorn/todolist/Controller.java
@@ -40,7 +40,7 @@ public class Controller {
             }
         });
 
-        todoListView.getItems().setAll(TodoData.getInsance().getTodoItems());
+        todoListView.setItems(TodoData.getInsance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
     }
@@ -68,11 +68,7 @@ public class Controller {
         if(result.isPresent() && result.get()==ButtonType.OK) {
             DialogController controller = fxmlLoader.getController();
             TodoItem newItem = controller.processResults();
-            todoListView.getItems().setAll(TodoData.getInsance().getTodoItems());
             todoListView.getSelectionModel().select(newItem);
-            System.out.println("OK pressed");
-        } else {
-            System.out.println("Cancel pressed");
         }
     }
 
